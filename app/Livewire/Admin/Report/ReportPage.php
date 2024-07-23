b6b38d8003a1858958f79539a7619bfe9bbe12d3
ytucayasi <saul.ytucayasi@gmail.com>
2024-07-23 17:14:22 -0500

    dist

--- app/Livewire/Admin/Report/ReportPage.php
+++ app/Livewire/Admin/Report/ReportPage.php
@@ -200,7 +200,10 @@ class ReportPage extends Component
 
     $this->totalCost = $query->sum('total_cost');
     $this->totalPack = $query->sum('people_count');
-    $this->totalProducts = $query->sum('total_products');
+    $this->totalProducts = /* $query->sum('total_products'); */
+      $query->join('inventories', 'reservations.id', '=', 'inventories.reservation_id')
+        ->selectRaw('COUNT(DISTINCT inventories.product_id) as total_products')
+        ->first()->total_products;
     $this->totalPaid = $query->where('payment_status', 1)->sum('total_pack');
     $this->total_ganado = $this->totalPaid - $this->totalCost;
   }
