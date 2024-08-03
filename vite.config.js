68023af8aac111b0462d82ced34027a30b434b44
Ytucayasi <saul.ytucayasi@gmail.com>
2024-08-03 00:53:10 -0500

    dev

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+const port = 5103;
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
 export default defineConfig({
   plugins: [
     laravel({
@@ -11,4 +12,10 @@ export default defineConfig({
       refresh: true,
     }),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: port,
+    strictPort: true,
+    origin: origin
+  }
 });
